7b0c3126e4b61f8d92140efaa25ca00b342b3da7
Didip Kerabat <didipk@gmail.com>
2015-05-21 20:04:44 -0700

    Able to parse RemoteIP

--- libstring/libstring.go
+++ libstring/libstring.go
@@ -1,6 +1,12 @@
 // Package libstring provides various string related functions.
 package libstring
 
+import (
+	"net/http"
+	"strings"
+)
+
+// StringInSlice finds needle in a slice of strings.
 func StringInSlice(sliceString []string, needle string) bool {
 	for _, b := range sliceString {
 		if b == needle {
@@ -8,4 +14,32 @@ func StringInSlice(sliceString []string, needle string) bool {
 		}
 	}
 	return false
+}
+
+func ipAddrFromRemoteAddr(s string) string {
+	idx := strings.LastIndex(s, ":")
+	if idx == -1 {
+		return s
+	}
+	return s[:idx]
+}
+
+// RemoteIP finds IP Address given http.Request struct.
+func RemoteIP(r *http.Request) string {
+	realIP := r.Header.Get("X-Real-IP")
+	forwardedFor := r.Header.Get("X-Forwarded-For")
+
+	if realIP == "" && forwardedFor == "" {
+		return ipAddrFromRemoteAddr(r.RemoteAddr)
+	}
+
+	if forwardedFor != "" {
+		// X-Forwarded-For is potentially a list of addresses separated with ","
+		parts := strings.Split(forwardedFor, ",")
+		for i, p := range parts {
+			parts[i] = strings.TrimSpace(p)
+		}
+		return parts[0]
+	}
+	return realIP
 }
--- libstring/libstring_test.go
+++ libstring/libstring_test.go
@@ -1,11 +1,35 @@
 package libstring
 
 import (
+	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestStringInSlice(t *testing.T) {
 	if StringInSlice([]string{"alice", "dan", "didip", "jason", "karl"}, "brotato") {
 		t.Error("brotato should not be in slice.")
+	}
+}
+
+func TestRemoteIP(t *testing.T) {
+	request, err := http.NewRequest("GET", "/", strings.NewReader("Hello, world!"))
+	if err != nil {
+		t.Errorf("Unable to create new HTTP request. Error: %v", err)
+	}
+
+	request.Header.Set("X-Real-IP", "2601:7:1c82:4097:59a0:a80b:2841:b8c8")
+
+	ip := RemoteIP(request)
+	if ip != "2601:7:1c82:4097:59a0:a80b:2841:b8c8" {
+		t.Errorf("Did not get the right IP. IP: %v", ip)
+	}
+
+	// X-Forwarded-For should have higher precedence.
+	request.Header.Set("X-Forwarded-For", "54.223.11.104")
+
+	ip = RemoteIP(request)
+	if ip != "54.223.11.104" {
+		t.Errorf("Did not get the right IP. IP: %v", ip)
 	}
 }
