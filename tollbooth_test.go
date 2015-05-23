cb97d82167a2ee3aa17d182fe1b13f034fe85d39
Didip Kerabat <didipk@gmail.com>
2015-05-23 07:43:58 -0700

    Add a lot more tests.

--- tollbooth_test.go
+++ tollbooth_test.go
@@ -191,3 +191,81 @@ func TestRequestMethodAndCustomHeadersBuildKeys(t *testing.T) {
 		}
 	}
 }
+
+func TestRequestMethodAndBasicAuthUsersBuildKeys(t *testing.T) {
+	limiter := NewLimiter(1, time.Second)
+	limiter.Methods = []string{"GET"}
+	limiter.BasicAuthUsers = []string{"bro"}
+
+	request, err := http.NewRequest("GET", "/", strings.NewReader("Hello, world!"))
+	if err != nil {
+		t.Errorf("Unable to create new HTTP request. Error: %v", err)
+	}
+
+	request.Header.Set("X-Real-IP", "2601:7:1c82:4097:59a0:a80b:2841:b8c8")
+	request.SetBasicAuth("bro", "tato")
+
+	for _, keys := range buildKeys(limiter, request) {
+		if len(keys) != 4 {
+			t.Errorf("Keys should be made of 4 parts. Keys: %v", keys)
+		}
+		for i, keyChunk := range keys {
+			if i == 0 && keyChunk != request.Header.Get("X-Real-IP") {
+				t.Errorf("The (%v) chunk should be remote IP. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 1 && keyChunk != request.URL.Path {
+				t.Errorf("The (%v) chunk should be request path. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 2 && keyChunk != "GET" {
+				t.Errorf("The (%v) chunk should be request method. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 3 && keyChunk != "bro" {
+				t.Errorf("The (%v) chunk should be basic auth user. KeyChunk: %v", i+1, keyChunk)
+			}
+		}
+	}
+}
+
+func TestRequestMethodCustomHeadersAndBasicAuthUsersBuildKeys(t *testing.T) {
+	limiter := NewLimiter(1, time.Second)
+	limiter.Methods = []string{"GET"}
+	limiter.Headers = make(map[string][]string)
+	limiter.Headers["X-Auth-Token"] = []string{"totally-top-secret", "another-secret"}
+	limiter.BasicAuthUsers = []string{"bro"}
+
+	request, err := http.NewRequest("GET", "/", strings.NewReader("Hello, world!"))
+	if err != nil {
+		t.Errorf("Unable to create new HTTP request. Error: %v", err)
+	}
+
+	request.Header.Set("X-Real-IP", "2601:7:1c82:4097:59a0:a80b:2841:b8c8")
+	request.Header.Set("X-Auth-Token", "totally-top-secret")
+	request.SetBasicAuth("bro", "tato")
+
+	for _, keys := range buildKeys(limiter, request) {
+		if len(keys) != 6 {
+			t.Errorf("Keys should be made of 4 parts. Keys: %v", keys)
+		}
+		for i, keyChunk := range keys {
+			if i == 0 && keyChunk != request.Header.Get("X-Real-IP") {
+				t.Errorf("The (%v) chunk should be remote IP. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 1 && keyChunk != request.URL.Path {
+				t.Errorf("The (%v) chunk should be request path. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 2 && keyChunk != "GET" {
+				t.Errorf("The (%v) chunk should be request method. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 3 && keyChunk != "X-Auth-Token" {
+				t.Errorf("The (%v) chunk should be request header. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 4 && (keyChunk != "totally-top-secret" && keyChunk != "another-secret") {
+				t.Errorf("The (%v) chunk should be request path. KeyChunk: %v", i+1, keyChunk)
+			}
+			if i == 5 && keyChunk != "bro" {
+				t.Errorf("The (%v) chunk should be basic auth user. KeyChunk: %v", i+1, keyChunk)
+			}
+		}
+	}
+
+}
