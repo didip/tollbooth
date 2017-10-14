b80d9db9bfe02236089a2056fa894bcf8773f539
Didip Kerabat <didipk@gmail.com>
2017-10-13 22:19:39 -0700

    update test for issue 48 to use different ip addresses

--- tollbooth_bug_report_test.go
+++ tollbooth_bug_report_test.go
@@ -1,8 +1,10 @@
 // +build slow
+// How to run: go test -tags=slow
 
 package tollbooth
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,8 +14,7 @@ import (
 )
 
 // See: https://github.com/didip/tollbooth/issues/48
-// How to run: go test -tags=slow
-func Test_Issue48_RequestTerminatedEvenOnLowVolume(t *testing.T) {
+func Test_Issue48_RequestTerminatedEvenOnLowVolumeOnSameIP(t *testing.T) {
 	lmt := limiter.New(nil).SetMax(20).SetTTL(time.Second)
 	lmt.SetMethods([]string{"GET"})
 
@@ -24,10 +25,44 @@ func Test_Issue48_RequestTerminatedEvenOnLowVolume(t *testing.T) {
 		w.Write([]byte(`hello world`))
 	}))
 
-	req, err := http.NewRequest("GET", "/doesntmatter", nil)
-	if err != nil {
-		t.Fatal(err)
+	// Why 11 times?
+	// Because 11 * 2 = 22, and our limit is 20.
+	// If the bug report is as what I understood, then this test is expected to break.
+	tries := 11
+	for i := 0; i < tries; i++ {
+		// Twice per second should not be limited
+		for j := 0; j < 2; j++ {
+			req, _ := http.NewRequest("GET", "/doesntmatter", nil)
+			req.RemoteAddr = "127.0.0.1"
+
+			rr := httptest.NewRecorder()
+			handler.ServeHTTP(rr, req)
+
+			//Should not be limited
+			if status := rr.Code; status != http.StatusOK {
+				t.Fatalf("Should be able to handle 20 reqs/second. HTTP status: %v. Expected HTTP status: %v", status, http.StatusOK)
+			}
+		}
+
+		time.Sleep(time.Second)
 	}
+
+	if limitReachedCounter > 0 {
+		t.Fatalf("We should never reached the limit, the counter should be 0. limitReachedCounter: %v", limitReachedCounter)
+	}
+}
+
+// See: https://github.com/didip/tollbooth/issues/48
+func Test_Issue48_RequestTerminatedEvenOnLowVolumeOnDifferentIPs(t *testing.T) {
+	lmt := limiter.New(nil).SetMax(20).SetTTL(time.Second)
+	lmt.SetMethods([]string{"GET"})
+
+	limitReachedCounter := 0
+	lmt.SetOnLimitReached(func(w http.ResponseWriter, r *http.Request) { limitReachedCounter++ })
+
+	handler := LimitHandler(lmt, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`hello world`))
+	}))
 
 	// Why 11 times?
 	// Because 11 * 2 = 22, and our limit is 20.
@@ -36,6 +71,9 @@ func Test_Issue48_RequestTerminatedEvenOnLowVolume(t *testing.T) {
 	for i := 0; i < tries; i++ {
 		// Twice per second should not be limited
 		for j := 0; j < 2; j++ {
+			req, _ := http.NewRequest("GET", "/doesntmatter", nil)
+			req.RemoteAddr = fmt.Sprintf("127.0.0.%v", i)
+
 			rr := httptest.NewRecorder()
 			handler.ServeHTTP(rr, req)
 
